a266e920d07744e378adc960d028bd82dd581019
ilona <gogiashvili.ilona@gmail.com>
2024-05-02 12:46:36 -0400

    commit tree e9c6b47c0caa025b30fff3070f10f271be8cb7a3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+  let numbers = record.find((r) => r.result === "W");
+  if (numbers) {
+    return numbers.year;
+  } else {
+    return undefined;
+  }
+}
